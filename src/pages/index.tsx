6a4540ee60abe9115b64ba5dd51d711ada03a9b7
Neung Darupong <darupong000@gmail.com>
2024-10-29 02:04:30 +0700

    feat: add content QUICKLY

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,11 @@
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import localFont from "next/font/local";
+import ImgLogo from "../../public/imgs/LogoCTP.png";
+import S1 from "../../public/imgs/s1.png";
+import PP from "../../public/imgs/poster.jpg";
+import P1 from "../../public/imgs/7.jpg";
+import P2 from "../../public/imgs/8.jpg";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -13,102 +19,148 @@ const geistMono = localFont({
 });
 
 export default function Home() {
+  const [timeLeft, setTimeLeft] = useState({
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  });
+
+  useEffect(() => {
+    const targetDate = new Date("November 9, 2024 00:00:00").getTime();
+
+    const updateCountdown = () => {
+      const now = new Date().getTime();
+      const distance = targetDate - now;
+
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      const hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      setTimeLeft({ days, hours, minutes, seconds });
+    };
+
+    const intervalId = setInterval(updateCountdown, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
+      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[auto_1fr_auto] items-center justify-items-center min-h-screen font-[family-name:var(--font-geist-sans)]`}
     >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/pages/index.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+      {/* Sticky Navbar */}
+      <nav className="navbar bg-base-100 shadow-lg sticky top-0 z-50 py-4 w-full">
+        <div className="container mx-auto flex justify-between items-center">
+          <div className="flex-1">
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              src={ImgLogo}
+              alt={""}
+              className="w-40 sm:w-48 md:w-60 h-auto"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          </div>
+          <div className="flex-none">
+            <button className="btn btn-outline btn-secondary text-xs sm:text-sm md:text-md lg:text-lg">
+              <a
+                href="https://forms.gle/PEpUHnLQwiEzxAMv8"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                ลงทะเบียนเข้าร่วมงาน ฟรี!
+              </a>
+            </button>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+      </nav>
+
+      {/* Main Content */}
+      <div className="flex flex-col row-start-2 items-center sm:items-start  p-0 pt-2 ">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+          <div className="flex flex-col justify-center items-center">
+            <Image
+              src={ImgLogo}
+              alt={""}
+              className="w-[300px] sm:w-[500px] md:w-[600px] lg:w-[800px] h-auto"
+            />
+          </div>
+          <div className="flex flex-col justify-center items-center">
+            <Image
+              src={S1}
+              alt={""}
+              className="w-[300px] sm:w-[500px] md:w-[600px] lg:w-[900px] h-auto"
+            />
+          </div>
+        </div>
+      </div>
+
+      {/* Countdown Section */}
+      <div className="bg-base-100 w-full h-auto py-10 flex flex-col items-center justify-center space-y-6">
+        <div>
+          <p className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold">
+            งานสัมมนาจะเริ่มใน
+          </p>
+        </div>
+        <div className="flex flex-wrap justify-center gap-4">
+          {Object.entries(timeLeft).map(([unit, value]) => {
+            const unitNames: { [key in keyof typeof timeLeft]: string } = {
+              days: "วัน",
+              hours: "ชั่วโมง",
+              minutes: "นาที",
+              seconds: "วินาที",
+            };
+            return (
+              <div
+                key={unit}
+                className="flex flex-col items-center justify-center border-2 border-gray-400 rounded-lg w-16 h-16 sm:w-32 sm:h-32 md:w-36 md:h-36 lg:w-40 lg:h-40"
+              >
+                <span className="text-3xl sm:text-4xl md:text-5xl lg:text-7xl font-bold">
+                  {value}
+                </span>
+                <span className="text-xs sm:text-sm md:text-md lg:text-lg">
+                  {unitNames[unit as keyof typeof timeLeft]}
+                </span>
+              </div>
+            );
+          })}
+        </div>
+        <div>
+          <button className="btn btn-outline btn-secondary btn-sm sm:btn-sm md:btn-md lg:btn-lg">
+            <a
+              href="https://forms.gle/PEpUHnLQwiEzxAMv8"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              ลงทะเบียนเข้าร่วมงาน
+            </a>
+          </button>
+        </div>
+      </div>
+      <div className="pt-10">
+        <Image
+          src={PP}
+          alt={""}
+          className="w-[300px] sm:w-[500px] md:w-[600px] lg:w-[900px] h-auto p-4"
+        />
+        <Image
+          src={P1}
+          alt={""}
+          className="w-[300px] sm:w-[500px] md:w-[600px] lg:w-[900px] h-auto p-4"
+        />
+        <Image
+          src={P2}
+          alt={""}
+          className="w-[300px] sm:w-[500px] md:w-[600px] lg:w-[900px] h-auto p-4"
+        />
+      </div>
+      <footer className="bg-gray-800 text-white w-full py-6 mt-10">
+        <div className="container mx-auto text-center">
+          <p className="text-sm md:text-md lg:text-lg">
+            © 2024 Social Media Technology, Rangsit University
+          </p>
+        </div>
       </footer>
     </div>
   );
